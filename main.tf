26c2e9fecbfa6b0d1c81aa1cfc00e0a4db50a7db
MuraliPuli <mm1501474@gmail.com>
2026-01-30 12:44:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,7 @@ resource "aws_instance" "name" {
     ami = "ami-0b6c6ebed2801a5cb"
     instance_type = "t3.micro"
     tags = {
-      Name="DEMO"
-    }
-  
-}
-resource "aws_vpc" "name" {
-    cidr_block = "10.0.0.0/16"
-    tags = {
-      Name ="vpc-K"
+      Name="DEMO-1"
     }
   
 }
